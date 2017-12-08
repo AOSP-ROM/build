6c9222b9cf2bde9fd03723ebee1c71a449d8b770
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-08 19:43:24 -0300

    soong_config: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY
    Change-Id: I5588c73cbf102aad153bddee331f9a5849353f53

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -85,6 +85,8 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
+	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo ''; \
 	echo '    "ArtUseReadBarrier": $(if $(filter false,$(PRODUCT_ART_USE_READ_BARRIER)),false,true),'; \
 	echo ''; \
