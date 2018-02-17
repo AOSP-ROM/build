be7c3cf2e3ba05eca93ffa1443dc24acd128d056
Paul Keith <javelinanddart@gmail.com>
2018-02-17 12:16:16 -0300

    soong_config: Add TARGET_LD_SHIM_LIBS

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -86,6 +86,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
+	echo '    "Target_shim_libs": "$(TARGET_LD_SHIM_LIBS)",'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
 	echo ''; \
