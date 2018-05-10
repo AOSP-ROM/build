LineageOS/android_build

core: config: Use host flex if requested
Change-Id: If87b54c4eab002a52a64b774d5d8b8e15036dd01

core/config.mk
@@ -552,7 +552,11 @@ prebuilt_sdk_tools_bin :=
 # Generic tools.
 JACK := $(HOST_OUT_EXECUTABLES)/jack
 
+ifeq ($(USE_HOST_BISON),yes)
+LEX := flex
+else
 LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
+endif
 # The default PKGDATADIR built in the prebuilt bison is a relative path
 # external/bison/data.
 # To run bison from elsewhere you need to set up enviromental variable

AOSP-ROM/build

core/config.mk
@@ -573,7 +573,11 @@ ZIPTIME := $(prebuilt_build_tools_bin)/ziptime
 # Generic tools.
 JACK := $(HOST_OUT_EXECUTABLES)/jack
 
+ifeq ($(USE_HOST_FLEX),yes)
+LEX := flex
+else
 LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
+endif
 # The default PKGDATADIR built in the prebuilt bison is a relative path
 # external/bison/data.
 # To run bison from elsewhere you need to set up enviromental variable